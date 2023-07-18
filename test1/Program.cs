7ca8166b15e397c0c24741cdda2a7d47f801eccf
Pavel <pavellast@mail.ru>
2023-07-18 21:33:54 +0300

    done?

---
+++ test1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Не знаю что не так
+
+
+Console.Clear();
+Console.WriteLine("Введите любое целые числа через пробел(отрицательные и положительные)");
+string[] st = (Console.ReadLine().Split(' '));
+Console.WriteLine(string.Join("", st));
+int arr = GetArrayFromString(st);
+int pos = Positive(arr);
+Console.WriteLine(arr);
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ");
+    int[] res = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+int Positive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ test2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int[] GetArray(int size, int minvalue, int maxvalue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minvalue, maxvalue + 1);
+    }
+    return res;
+}
+int[] CopyArray(int[]array)
+{
+int[] arr = new int[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    
+    arr[i] = array[i];
+}
+return arr;
+}
+int[] ar = GetArray(5, 0, 20);
+Console.WriteLine(string.Join("", ar));
+int[] copy = CopyArray(ar);
+Console.WriteLine(string.Join("", copy));
